364d54e75f49c5813296a51b012aa88e1ffe4cce
erin williams <erin.cwilliams0@gmail.com>
2025-09-23 15:35:01 -0500

    Add project files.

---
+++ MessageWallAPI/Controllers/MessageWallController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MessageWallAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageWallController : ControllerBase
+    {
+        // GET: api/<MessageWallController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            List<string> output = new List<string>
+            {
+                "Hello World",
+                "How are you"
+            };
+
+            return output;
+        }
+
+        // GET api/<MessageWallController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MessageWallController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MessageWallController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MessageWallController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
